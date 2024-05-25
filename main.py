9b6b17d1853667164de7d30217738579fb5b1e4b
Waki Yuku <0407yuuku@gmail.com>
2024-05-25 15:06:26 +0900

    add classificater and decisiontree

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ num_dim = 6 #６軸データ
 columns_list = ["accel x", "accel y", "accel z", "pitch", "roll", "yaw"]
 dir_names = ['waki2s', 'waki2u', 'waki2f']
 
-#実験データの読み込み
+#実験データのcsvを読み込み
 datas = {}
 for dir_name in dir_names:
     directory_path = "data/" + dir_name
     datas[dir_name] = get_trial_csv_files(directory_path)
 
-
+classifier = Classifier(cv=10, columns_list = columns_list, save_path="results") #分類器の定義
+#データを取得し加工、分類器への追加
 for name, data_frames in datas.items():
     #取り合えず100フレームで統一残りはゼロパディング
     data_array = np.zeros((len(data_frames), 120, num_dim))
@@ -21,17 +22,25 @@ for name, data_frames in datas.items():
         data = df[columns_list].to_numpy()
         data_len = data.shape[0]
         data_array[i, :data_len, :] = data
-
     #データの加工
-    imu = IMU_data(name, data_array, thred_rasio=0.2, num_roll_back= 10)
-    plt.plot(np.mean(imu.cliped_datas, axis = 0), label = columns_list)
-    plt.title(name)
-    plt.legend()
+    imu = IMU_data(name, data_array,cutoff_freq=25, thred_rasio=0.3, num_roll_back= 5)
+    #加工後のデータの表示
+    fig, axes = plt.subplots(6, 1, figsize=(8, 16)) 
+    for i, ax in enumerate(axes):
+        #ax.plot(np.mean(imu.normalized_datas, axis = 0)[:, i], label = columns_list[i])
+        ax.plot(imu.cliped_datas[:, :,  i].T)
+        ax.set_title(columns_list[i])
+    plt.suptitle(name)
+    plt.tight_layout()
     plt.savefig("results/figure/mean_{}.png".format(name))
     plt.close()
-    #plt.show()
+    #分類器にデータの追加
+    classifier.append_data_array(imu.normalized_datas, name)
+
+#決定木での分類
+classifier.fit_decision_tree()
 
 
-
+print(classifier.clossval_scores)
 
 
--- util.py
+++ util.py
@@ -5,6 +5,18 @@ import fnmatch
 import os
 import scipy.signal as signal
 
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, cross_validate
+
+#下記は決定木可視化のためのツール
+import graphviz
+import pydotplus
+from IPython.display import Image
+from io import StringIO
+
 def get_trial_csv_files(directory):
     # ディレクトリ内のすべてのファイル名を取得
     files = os.listdir(directory)
@@ -34,6 +46,9 @@ class IMU_data:
         self.filterd_datas = self.filtering_data(self.law_datas)
         self.norm_datas    = self.culc_acc_norm(self.filterd_datas)
         self.cliped_datas  = self.clip_by_threshold(self.filterd_datas)
+        self.normalized_datas = self.get_normalize_data(self.cliped_datas)
+        # self.norm_datas    = self.culc_acc_norm(self.law_datas)
+        # self.cliped_datas  = self.clip_by_threshold(self.law_datas)
 
     def filtering_data(self, data):
         #3次のバターワースフィルター
@@ -56,7 +71,7 @@ class IMU_data:
         indices = []
         for norm in self.norm_datas:
             threshold = np.max(norm)*self.thred_rasio
-            if np.sum(norm > threshold):
+            if np.sum(norm >= threshold):
                 #閾値よりも大きくなるインデックスを取得
                 indices.append(np.where(norm > threshold)[0])
             else:
@@ -70,8 +85,74 @@ class IMU_data:
             cliped_datas[i, :, :] = data[i, frame:frame + 40, :]
         return cliped_datas
     
+    def get_normalize_data(self, data):
+        #最大値を求める
+        max = np.max(data, axis=1, keepdims=True)
+        min = np.min(data, axis=1, keepdims=True)
+        #正規化
+        normalize_datas = (data - min) / (max - min)
+        return normalize_datas
+    
     def get_filterd_data(self):
         return self.filterd_datas
 
 
+class Classifier:
+    def __init__(self, test_split_rasio = 0.2, cv = 10, columns_list = [], save_path = None):
+        self.datas = None
+        self.labels = None
+        self.train_datas = None
+        self.train_labels = None
+        self.test_datas = None
+        self.train_labels = None
+        self.test_split_rasio = test_split_rasio
+        self.cv = cv
+        self.culumns_list = columns_list
+        self.clossval_scores = {}
+        self.save_path = save_path
 
+    def append_data_array(self, data, class_name):
+        num_sample = data.shape[0]
+        data = data.reshape(num_sample, -1)
+        labels = np.full(num_sample, class_name)
+        train_datas, test_datas, train_labels, test_labels = train_test_split(data, labels, test_size = self.test_split_rasio)
+        if np.any(self.train_datas) == None:
+            self.datas = data
+            self.labels = labels
+            self.train_datas, self.test_datas = train_datas, test_datas
+            self.train_labels, self.test_labels = train_labels, test_labels
+        else:
+            self.datas = np.concatenate((self.datas, data))
+            self.labels = np.concatenate((self.labels, labels))
+            self.train_datas = np.concatenate((self.train_datas, train_datas), axis=0)
+            self.test_datas = np.concatenate((self.test_datas, test_datas), axis=0)
+            self.train_labels = np.concatenate((self.train_labels, train_labels))
+            self.test_labels = np.concatenate((self.test_labels, test_labels))
+
+    def fit_decision_tree(self):
+        clf = DecisionTreeClassifier()
+        #交差検証
+        scores = cross_val_score(clf, self.datas, self.labels, cv = self.cv)
+        self.clossval_scores["DecisionTree:"] = scores.mean()
+
+        # クロスバリデーションを実行して各分割の重要度を取得
+        cv_results = cross_validate(clf, self.datas, self.labels, cv=self.cv, return_estimator=True)
+        importances = []
+        for estimator in cv_results['estimator']:
+            importances.append(estimator.feature_importances_)
+
+        # 重要度の平均を計算
+        feature = np.mean(importances, axis=0)
+        clf = clf.fit(self.train_datas, self.train_labels)
+        feature = np.array(feature).reshape(6, -1)
+        feature = np.sum(feature, axis=1)
+        plt.barh(range(len(feature)), feature)
+        plt.yticks(range(len(feature)), self.culumns_list, fontsize=14)
+        plt.savefig("{}/figure/decision_tree_mean_feature.png".format(self.save_path))
+
+        #訓練データの精度
+        tree.plot_tree(clf)
+        plt.savefig("{}/figure/decision_tree.png".format(self.save_path))
+        plt.close()
+
+        
